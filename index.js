856da06ba86f257f932b9599e826f7221ce42ddc
Ian McGregor <ianmcgregor@Ians-MacBook-Pro.local>
2019-07-25 23:03:08 -0400

    add headers

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ var path = require('path');
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
+
+  var headers = {};
+  headers['content-type'] = 'text/html';
+  headers["Access-Control-Allow-Origin"] = "*";
+  headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+  headers["Access-Control-Allow-Credentials"] = false;
+  headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+  headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+
   var result = {
     statusCode: 200,
     body: contents.toString(),
-    headers: {'content-type': 'text/html'}
+    headers: headers
   };
 
   callback(null, result);
